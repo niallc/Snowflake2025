22cd9caf4e17968144c699fc469ab9de9cb83b9e
Niall Cardin <NiallC@gmail.com>
2025-07-11 01:07:25 +0100

    Wokring on first dummy training.

--- train_example.py
+++ train_example.py
@@ -16,29 +16,48 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def create_dummy_data(num_samples: int = 100) -> list:
-    """Create dummy training data for testing."""
+    """Create dummy training data for testing, ensuring no duplicate moves."""
     dummy_data = []
     
-    # Create some simple trmph strings
+    # All possible moves for 13x13
+    letters = 'abcdefghijklm'
+    numbers = [str(i+1) for i in range(13)]
+    all_moves_13 = [l+n for l in letters for n in numbers]
+    # All possible moves for 7x7
+    letters7 = 'abcdefg'
+    numbers7 = [str(i+1) for i in range(7)]
+    all_moves_7 = [l+n for l in letters7 for n in numbers7]
+    
     base_patterns = [
-        "http://www.trmph.com/hex/board#13,a1b2c3",
-        "http://www.trmph.com/hex/board#13,a1b2c3d4e5f6g7",
-        "http://www.trmph.com/hex/board#13,a1b2c3d4e5f6g7h8i9j10k11l12m13",
-        "http://www.trmph.com/hex/board#11,a8h1b8h2c8h3d8h4e8h5f8h6g8h7a7a6b6b5c5c4d4d3e3e2f2f1g1",  # Blue winner
-        "http://www.trmph.com/hex/board#11,a8h1b8h2c8h3d8h4e8h5f8h6g8h7b1a1a3a2c1b2c2b3a4c3b4d3e3d4c5e4f4e5d6f5g5f6g6f7e7g7",  # Red winner
+        ("http://www.trmph.com/hex/board#13,", ["a1", "b2", "c3"]),
+        ("http://www.trmph.com/hex/board#13,", ["a1", "b2", "c3", "d4", "e5", "f6", "g7"]),
+        ("http://www.trmph.com/hex/board#13,", ["a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8", "i9", "j10", "k11", "l12", "m13"]),
+        ("http://www.trmph.com/hex/board#11,", ["a8", "h1", "b8", "h2", "c8", "h3", "d8", "h4", "e8", "h5", "f8", "h6", "g8", "h7", "a7", "a6", "b6", "b5", "c5", "c4", "d4", "d3", "e3", "e2", "f2", "f1", "g1"]),
+        ("http://www.trmph.com/hex/board#11,", ["a8", "h1", "b8", "h2", "c8", "h3", "d8", "h4", "e8", "h5", "f8", "h6", "g8", "h7", "b1", "a1", "a3", "a2", "c1", "b2", "c2", "b3", "a4", "c3", "b4", "d3", "e3", "d4", "c5", "e4", "f4", "e5", "d6", "f5", "g5", "f6", "g6", "f7", "e7", "g7"]),
     ]
     
     for i in range(num_samples):
-        # Randomly select and modify a base pattern
-        pattern = np.random.choice(base_patterns)
-        # Add some random moves to make it unique
-        if "a1b2c3" in pattern:
-            extra_moves = "d4e5f6" if i % 2 == 0 else "g7h8i9"
-            pattern = pattern.replace("a1b2c3", f"a1b2c3{extra_moves}")
-        
-        dummy_data.append(pattern)
+        base_url, base_moves = base_patterns[np.random.randint(len(base_patterns))]
+        moves = list(base_moves)
+        if base_url.endswith('#13,'):
+            available_moves = set(all_moves_13) - set(moves)
+        else:
+            available_moves = set(all_moves_7) - set(moves)
+        # Add up to 3 extra unique moves
+        extra_moves = np.random.choice(list(available_moves), size=min(3, len(available_moves)), replace=False)
+        moves.extend(extra_moves)
+        trmph = base_url + ''.join(moves)
+        dummy_data.append(trmph)
     
     return dummy_data
+
+def log_epoch_summary(epoch, train_loss, val_loss):
+    """Utility to log epoch summary with proper formatting."""
+    if val_loss is not None:
+        val_str = f"{val_loss:.4f}"
+    else:
+        val_str = "N/A"
+    logger.info(f"Epoch {epoch}: Train Loss: {train_loss:.4f}, Val Loss: {val_str}")
 
 def main():
     """Main training function."""
@@ -72,7 +91,7 @@ def main():
         epoch = epoch_data['epoch']
         train_loss = epoch_data['train']['total_loss']
         val_loss = epoch_data['val']['total_loss'] if epoch_data['val'] else None
-        logger.info(f"Epoch {epoch}: Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f if val_loss else 'N/A'}")
+        log_epoch_summary(epoch, train_loss, val_loss)
 
 if __name__ == "__main__":
     main() 
