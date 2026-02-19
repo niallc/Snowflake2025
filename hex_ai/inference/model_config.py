804927a1a763476c564f201f024eedc30b48dd0f
Niall Cardin <NiallC@gmail.com>
2026-02-19 04:45:53 +0000

    Updated to most recent model (not chosen by tournament).

--- hex_ai/inference/model_config.py
+++ hex_ai/inference/model_config.py
@@ -233,7 +233,7 @@ MODEL_GENERATIONS: Dict[int, Dict[str, Any]] = {
     },
     52: {
         "dir": "hyperparameter_tuning/pipeline_20260217_232105/",
-        "models": ["epoch158_mini48.pt.gz", "epoch158_mini15.pt.gz", "epoch157_mini89.pt.gz"]
+        "models": ["epoch158_mini50.pt.gz", "epoch158_mini15.pt.gz", "epoch157_mini89.pt.gz"]
     },
 }
 
