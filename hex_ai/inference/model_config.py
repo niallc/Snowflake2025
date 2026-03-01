5db61e302607a5b77fc2d04cb1b3b6dbf05de397
Niall Cardin <NiallC@gmail.com>
2026-03-01 16:08:37 +0000

    Minor model update.

--- hex_ai/inference/model_config.py
+++ hex_ai/inference/model_config.py
@@ -245,7 +245,7 @@ MODEL_GENERATIONS: Dict[int, Dict[str, Any]] = {
     },
     55: {
         "dir": "hyperparameter_tuning/pipeline_20260227_225222/",
-        "models": ["epoch168_mini30.pt.gz", "epoch166_mini84.pt.gz"]
+        "models": ["epoch168_mini18.pt.gz", "epoch166_mini84.pt.gz"]
     },
 }
 
